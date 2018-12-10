a35e1937f8cad660ce8b98b8dec449d61f963d87
AatishMolasi <aatish.molasi@lithium.com>
2018-12-10 22:04:29 +0530

    Deleting derived data since it was interfereing with the diff

--- DerivedData10/ImageCacheDemo/Build/Intermediates.noindex/ImageCacheManager.build/Debug-iphonesimulator/ImageCacheManager.build/Objects-normal/x86_64/ImageCacheManager_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aatish.molasi/Documents/Development/iOs/mindvalley/ImageCacheDemo/DerivedData10/ImageCacheDemo/Build/Intermediates.noindex/ImageCacheManager.build/Debug-iphonesimulator/ImageCacheManager.build/DerivedSources/ImageCacheManager_vers.c
--- DerivedData10/ImageCacheDemo/Build/Intermediates.noindex/ImageCacheManager.build/Debug-iphonesimulator/ImageCacheManagerTests.build/DerivedSources/ImageCacheManagerTests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ImageCacheManagerTestsVersionString[];
- extern const double ImageCacheManagerTestsVersionNumber;
-
- const unsigned char ImageCacheManagerTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ImageCacheManagerTests  PROJECT:ImageCacheManager-1" "\n";
- const double ImageCacheManagerTestsVersionNumber __attribute__ ((used)) = (double)1.;
