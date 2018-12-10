14d0e72056c1977ba132093ec8b5f634aa30ba0b
AatishMolasi <aatish.molasi@lithium.com>
2018-12-10 14:35:29 +0530

    Setting up the pin manager tests

--- ImageCacheDemo/Classes/AsyncImageLoader/Interactor/PinManager.swift
+++ ImageCacheDemo/Classes/AsyncImageLoader/Interactor/PinManager.swift
@@ -1,16 +1,20 @@
 import Foundation
 
 class PinManager {
-    static let sharedManager = PinManager()
+    static let sharedManager = PinManager(urlSession: URLSession.shared)
+    let urlSession: URLSession
+
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
 
     func getPins(page: Int, completion: @escaping ([Pin]?, Error?)->()) {
         if let url = URL(string:"http://pastebin.com/raw/wgkJgazE") {
-            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let task = self.urlSession.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print("Fetch operation failed with error : \(String(describing: error))")
                     completion(nil, error)
                 }
-                print("This is the data : \(String(describing: data))")
                 do {
                     if let safeData = data {
                         let result = try  JSONDecoder().decode([Pin].self, from: safeData)
---
+++ ImageCacheDemoTests/PasteboardImagesViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  PasteboardImagesViewControllerTests.swift
+//  ImageCacheDemoTests
+//
+//  Created by Aatish Molasi on 12/10/18.
+//  Copyright © 2018 Aatish Molasi. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class PasteboardImagesViewControllerTests: XCTestCase {
+    
+}
